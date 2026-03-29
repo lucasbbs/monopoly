b29ccf032d0c214b82405e77266d8865f5eabb1d
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2026-03-29 15:34:24 -0400

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'welcome')->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::view('monopoly', 'game')->name('monopoly');
+});
+
+require __DIR__.'/settings.php';
