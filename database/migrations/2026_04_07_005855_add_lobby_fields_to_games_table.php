b7006d0a81157c9320fe8b9e4807198781c34430
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2026-04-06 21:19:39 -0400

    feat(database): add game lobby columns

---
+++ database/migrations/2026_04_07_005855_add_lobby_fields_to_games_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->string('name')->default('Untitled Game')->after('id');
+            $table->unsignedTinyInteger('max_players')->default(1)->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->dropColumn(['name', 'max_players']);
+        });
+    }
+};
